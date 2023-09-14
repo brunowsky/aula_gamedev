c995be831f62f4fba945946944665491d91d1a8f
brunowsky <brunodf1998@hotmail.com>
2023-09-13 22:07:52 -0300

    note -> pc background while choosing upgrades

--- game/scenes/ui/ability_upgrade_card.gd
+++ game/scenes/ui/ability_upgrade_card.gd
@@ -5,6 +5,8 @@ signal selected
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
 
+var disabled = false
+
 
 func _ready():
 	gui_input.connect(on_gui_input)
@@ -17,15 +19,37 @@ func play_in(delay: float = 0):
 	$AnimationPlayer.play("in")
 
 
+func play_discard():
+	$AnimationPlayer.play("discard")
+
+
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
 
 
-func on_gui_input(event: InputEvent):
-	if event.is_action_pressed("left_click"):
+func select_card():
+		disabled = true
+		$AnimationPlayer.play("selected")
+		
+		for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+			if other_card == self:
+				continue
+			other_card.play_discard()
+			
+		
+		await $AnimationPlayer.animation_finished
 		selected.emit()
+
+func on_gui_input(event: InputEvent):
+	if disabled:
+		return
+		
+	if event.is_action_pressed("left_click"):
+		select_card()
 
 
 func on_mouse_entered():
+	if disabled:
+		return
 	$HoverAnimationPlayer.play("hover")
