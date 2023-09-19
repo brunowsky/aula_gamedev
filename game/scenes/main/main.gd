87758481b72a9fcb6a7e27d28efb570504a51cf5
brunowsky <brunodf1998@hotmail.com>
2023-09-18 22:11:32 -0300

    note -> pc udemy down

--- game/scenes/main/main.gd
+++ game/scenes/main/main.gd
@@ -11,7 +11,9 @@ func _ready():
 	
 
 func _unhandled_input(event):
-	
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_scene.instantiate())
+		get_tree().root.set_input_as_handled()
 
 
 func on_player_died():
