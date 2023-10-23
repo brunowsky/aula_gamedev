9f8919593e5ff3d8836ededaca64a2026484ada5
brunowsky <brunodf1998@hotmail.com>
2023-10-23 19:04:58 -0300

    pc -> notebook

--- game/scenes/autoload/meta_progression.gd
+++ game/scenes/autoload/meta_progression.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SAVE_FILE_PATH = "user://game.save"
+
 var save_data: Dictionary = {
 	"meta_upgrade_currency": 0,
 	"meta_upgrades": {}
@@ -8,15 +10,29 @@ var save_data: Dictionary = {
 
 func _ready():
 	GameEvents.experience_vial_collected.connect(on_experience_collected)
-	add_meta_upgrade(load("res://resources/meta_upgrades/experience_gain.tres"))
+	load_save_file()
 	
+	
+func load_save_file():
+	if !FileAccess.file_exists(SAVE_FILE_PATH):
+		return
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+	save_data = file.get_var()
+	
+
+func save():
+	var file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	file.store_var(save_data)
+
 
 func add_meta_upgrade(upgrade: MetaUpgrade):
 	if !save_data["meta_upgrades"].has(upgrade.id):
 		save_data["meta_upgrades"][upgrade.id] = {
 			"quantity": 0
 		}
+		
 	save_data["meta_upgrades"][upgrade.id]["quantity"] += 1
+	save()
 
 
 func on_experience_collected(number: float):
